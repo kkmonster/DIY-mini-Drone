c327fcbab5ff5a4ef4895449bb37fea7eaf23873
kkmonster <kkmonster@hotmail.com>
2014-11-05 10:05:24 +0700

    Change Speed Controller loop to 250 Hz

--- Git_drone/drone_v2/Src/stm32f0xx_it.c
+++ Git_drone/drone_v2/Src/stm32f0xx_it.c
@@ -77,7 +77,7 @@ void EXTI4_15_IRQHandler(void)
   /* USER CODE BEGIN EXTI4_15_IRQn 1 */
 	iqs_count++;
 	
-	if(iqs_count > 2){
+	if(iqs_count > 2){              // 250 Hz
 		iqs_count=0 		;
 		Interrupt_call()	;
 	}
